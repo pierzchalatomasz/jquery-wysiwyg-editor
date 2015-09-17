f1594c5051602c189fca33d3976bfdfc7d21517e
tomik3690 <tomik031195@gmail.com>
2015-09-17 08:21:37 +0200

    Added minify task to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,8 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
-// var concat = require('gulp-concat');
-// var uglify = require('gulp-uglify');
-// var wrap = require("gulp-wrap");
+var uglify = require('gulp-uglify');
+var wrap = require("gulp-wrap");
 var LessPluginAutoPrefix = require('less-plugin-autoprefix'),
 autoprefix= new LessPluginAutoPrefix({ browsers: ["last 3 versions"] });
 
@@ -16,20 +15,10 @@ gulp.task('less', function () {
   .pipe(gulp.dest('dist/css'));
 });
 
-// gulp.task('plugins', function() {
-//   return gulp.src('js/plugins/*.js')
-//   .pipe(concat('plugins.js'))
-//   .pipe(gulp.dest('../js/'))
-//   .pipe(uglify())
-//   .pipe(wrap('/*\n**\n** CarWash Plugins\n**\n*/\n\n<%= contents %>'))
-//   .pipe(gulp.dest('../js/'));
-// });
-//
-// gulp.task('minify', function() {
-//   return gulp.src('../js/main.js')
-//   .pipe(gulp.dest('../js/'))
-//   .pipe(wrap('(function(){\n"use strict";\n<%= contents %>\n})();'))
-//   .pipe(uglify())
-//   .pipe(wrap('/*\n**\n** CarWash Scripts\n**\n*/\n\n<%= contents %>'))
-//   .pipe(gulp.dest('../js/'));
-// });
+gulp.task('minify', function() {
+  return gulp.src('dist/js/jquery.wysiwygEditor.js')
+  .pipe(gulp.dest('../js/'))
+  .pipe(uglify())
+  .pipe(wrap('/*\n**\n** jquery.wysiwygEditor.min.js \n**\n*/\n\n<%= contents %>'))
+  .pipe(gulp.dest('dist/js/jquery.wysiwygEditor.min.js'));
+});
